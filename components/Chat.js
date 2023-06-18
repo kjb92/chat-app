ac68833d2d0c383cfc8e89433ea5abcc551134a3
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-06-18 12:19:13 +0200

    feat: Implement gifted chat

--- components/Chat.js
+++ components/Chat.js
@@ -1,25 +1,51 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
 
 const Chat = ({ route, navigation }) => {
+  //Get username and background color form route parameters
   const { username, backgroundColor } = route.params;
+  //Messages state
+  const [messages, setMessages] = useState([]);
  
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+  };
+
+  //Set navigation title to username
   useEffect(() => {
     navigation.setOptions({ title: username });
   }, []);
 
+  //Pull in messages; sample messages for now
+  useEffect(()=> {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
+  }, []);
+
+  //Render chat UI
   return (
-   <View 
-    style={[
-      styles.container, 
-      {backgroundColor: backgroundColor},
-    ]}
-    >
-   </View>
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1
+      }}
+    />
  );
 }
 
+//StyleSheet for Chat.js
 const styles = StyleSheet.create({
  container: {
    flex: 1,
