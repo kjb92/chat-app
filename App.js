740d4c4cb6512db16d1c3438de4021a4cc2d418f
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-06-18 17:33:07 +0200

    chore: Add firebase config

--- App.js
+++ App.js
@@ -5,11 +5,30 @@ import Chat from './components/Chat';
 // import react Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//import Cloud Firestore
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 // Create the navigator
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  // This web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAvby18AjPtHhcQ1muO7PNCNa3uxr5GuHE",
+    authDomain: "chatapp-70cc1.firebaseapp.com",
+    projectId: "chatapp-70cc1",
+    storageBucket: "chatapp-70cc1.appspot.com",
+    messagingSenderId: "302452061868",
+    appId: "1:302452061868:web:7b6f2c0bae271f9d675109"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
