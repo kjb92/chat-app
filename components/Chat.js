9e8ff2427dfa05a039c16a4d6f7ddf83345a19a2
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-06-19 08:58:11 +0200

    feat: Pass message on to Firestore db

--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
-import { StyleSheet, View, KeyboardAvoidingView, Platform } from 'react-native';
+import { StyleSheet, View, KeyboardAvoidingView, Platform, Alert } from 'react-native';
 import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 //Import get data functions from Firestore
-import { collection, getDocs } from "firebase/firestore";
+import { collection, doc, getDocs, addDoc, setDoc, Timestamp } from "firebase/firestore";
 import { async } from '@firebase/util';
 
 const Chat = ({ route, navigation, db }) => {
@@ -10,10 +10,15 @@ const Chat = ({ route, navigation, db }) => {
   const { username, backgroundColor } = route.params;
   //Messages state
   const [messages, setMessages] = useState([]);
-   //Send function
+  
+  //Send function
   const onSend = (newMessages) => {
+    //TBD: addDoc in Forestore
+    addMessage(newMessages[0]);
+    //Update messages
     setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
-  };
+    };
+    
   //Chat bubble customization
   const renderBubble = (props) => {
     return <Bubble
@@ -44,6 +49,17 @@ const Chat = ({ route, navigation, db }) => {
     setMessages(newMessages);
   };
 
+  //ASYNC: Add message
+  const addMessage = async (newMessage) => {
+    const newMessageRef = await addDoc(collection(db, "messages"), newMessage);
+    if (newMessageRef.id) {
+      //Delete after test phase for better UX
+      Alert.alert(`New message ${newMessage.text} added`);
+    } else {
+      Alert.alert(`Unable to add message. Please try later`);
+    }
+  };
+
   //Set navigation title to username
   useEffect(() => {
     navigation.setOptions({ title: username });
@@ -69,7 +85,7 @@ const Chat = ({ route, navigation, db }) => {
       {Platform.OS === 'android' ? <KeyboardAvoidingView behavior='height' /> : null}
     </View>
  );
-}
+};
 
 //StyleSheet for Chat.js
 const styles = StyleSheet.create({
