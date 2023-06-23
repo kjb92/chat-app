38bd463884333cd2a81ac679d85f7c98485ca87b
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-06-23 16:25:42 +0200

    fix: Update GiftedChat user prop

--- components/Chat.js
+++ components/Chat.js
@@ -97,7 +97,8 @@ const Chat = ({ route, navigation, db }) => {
         renderBubble={renderBubble}
         onSend={messages => onSend(messages)}
         user={{
-          _id: 1
+          _id: userID,
+          name: username
         }}
       />
       {/* Keyboard-Fix for android */}
